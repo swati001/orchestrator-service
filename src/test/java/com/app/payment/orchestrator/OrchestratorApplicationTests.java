fe5929f63311ea3999602e13bc41f8dc92ca8f5f
Swati Sharma <swatiinfo86@gmail.com>
2020-04-21 23:01:00 +0530

    added files

---
+++ src/test/java/com/app/payment/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.payment.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
