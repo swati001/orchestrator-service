7f1482c52b21477af40d3a9c6e07a43927bfe936
Swati Sharma <swatiinfo86@gmail.com>
2020-04-22 12:11:12 -0400

    Update OrchestratorService.java

--- src/main/java/com/app/payment/orchestrator/service/OrchestratorService.java
+++ src/main/java/com/app/payment/orchestrator/service/OrchestratorService.java
@@ -1,5 +1,8 @@
 package com.app.payment.orchestrator.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Component
 public class OrchestratorService {
