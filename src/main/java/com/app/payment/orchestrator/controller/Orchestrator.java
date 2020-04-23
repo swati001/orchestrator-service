b10187ca27dcf24ac85b3daf3bf465937c627c98
Swati Sharma <swatiinfo86@gmail.com>
2020-04-23 13:24:33 -0400

    Update Orchestrator.java

--- src/main/java/com/app/payment/orchestrator/controller/Orchestrator.java
+++ src/main/java/com/app/payment/orchestrator/controller/Orchestrator.java
@@ -5,13 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/payment")
 public class Orchestrator {
 
-@GetMapping("/get")
-public ResponseEntity<String> getPayment(){
-  return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
-}
+  @Autowired
+  private OrchestratorService orchestratorService;
+  
+  @GetMapping("/get")
+  public ResponseEntity<String> getPayment(){
+    return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+  }
 }
